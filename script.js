afe8483728fff0d17f1c13a4309dbd60d1f08b66
aseel7marwan <107547643+aseel7marwan@users.noreply.github.com>
2024-12-31 03:45:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// Show additional message when button is clicked
+// Show the envelope and message when the button is clicked
 function showMessage() {
     const newMessage = document.getElementById("newMessage");
-    newMessage.classList.remove("hidden"); // إزالة الكلاس المخفي
-    newMessage.classList.add("showMessage"); // إضافة الكلاس لإظهار الرسالة
+    const envelope = document.querySelector(".envelope"); // تحديد الصندوق
+    newMessage.classList.remove("hidden");
+    newMessage.classList.add("showMessage");
+
+    envelope.classList.remove("hidden");  // إزالة الكلاس hidden من الصندوق لإظهاره
+    envelope.classList.add("showMessage");
+
     window.scrollTo({
         top: document.body.scrollHeight, // Scroll to the bottom after revealing content
         behavior: "smooth",
