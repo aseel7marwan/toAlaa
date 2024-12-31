540b65f6be26a6b5875fd2818b17b3cee7c568d5
aseel7marwan <107547643+aseel7marwan@users.noreply.github.com>
2024-12-31 03:57:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Show additional message when the box is clicked
+// Show additional message when the clickable text is clicked
 function showMessage() {
     const newMessage = document.getElementById("newMessage");
-    const instructionsBox = document.getElementById("instructionsBox");
+    const instructions = document.getElementById("instructions");
 
     newMessage.classList.remove("hidden");
     newMessage.classList.add("showMessage");
-    instructionsBox.style.display = "none"; // Hide the box after clicking
+    instructions.style.display = "none"; // Hide the clickable text after it is clicked
 
     window.scrollTo({
         top: document.body.scrollHeight, // Scroll to the bottom after revealing content
